6289f440eb7d96a7b4ff3617fee5f24ae14887de
Dan F-M <danfm@nyu.edu>
2013-06-15 00:24:48 -0400

    getting word list

---
+++ gen_word_list.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+__all__ = []
+
+import re
+import nltk
+import redis
+import requests
+from bs4 import BeautifulSoup
+from multiprocessing import Pool
+from nltk.tokenize import PunktSentenceTokenizer
+
+
+tokenizer = PunktSentenceTokenizer()
+stripper = re.compile(r"^[a-zA-Z]")
+
+redis_pool = redis.ConnectionPool()
+
+
+def run_one(*args):
+    # Redis setup.
+    rdb = redis.Redis(connection_pool=redis_pool)
+    pipe = rdb.pipeline(transaction=False)
+
+    # Download a random article.
+    url = "http://en.wikipedia.org/wiki/Special:Random"
+    r = requests.get(url)
+    if r.status_code != requests.codes.ok:
+        print("Returned {0}. Re-trying".format(r.status_code))
+        return run_one()
+    print(r.url)
+
+    # Parse and walk the document.
+    tree = BeautifulSoup(r.text)
+    for el in tree.find(id="mw-content-text").find_all("p"):
+        for s in tokenizer.tokenize(el.text):
+            for t in nltk.pos_tag(nltk.word_tokenize(s)):
+                if t[1][0] == "N":
+                    w = re.sub(r"[0-9,.;?\[\]\(\)]", "", t[0].lower())
+                    if len(w) > 1:
+                        pipe.zincrby("puppy:words", w, 1)
+    pipe.execute()
+
+
+if __name__ == "__main__":
+    pool = Pool()
+    while True:
+        pool.map(run_one, range(10 * pool._processes))

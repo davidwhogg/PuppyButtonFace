6fe8274e7d896dc12ebf364f60822b59dc42a919
Dan F-M <danfm@nyu.edu>
2016-05-19 19:40:16 -0700

    updated word list

--- gen_word_list.py
+++ gen_word_list.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import (division, print_function, absolute_import,
-                        unicode_literals)
-
-__all__ = []
+from __future__ import division, print_function
 
 import re
-import nltk
-import redis
 import requests
-from bs4 import BeautifulSoup
+from collections import Counter
 from multiprocessing import Pool
+
+from bs4 import BeautifulSoup
+
+import nltk
 from nltk.tokenize import PunktSentenceTokenizer
+
+__all__ = []
 
 
 tokenizer = PunktSentenceTokenizer()
 stripper = re.compile(r"^[a-zA-Z]")
 
-redis_pool = redis.ConnectionPool()
-
 
 def run_one(*args):
-    # Redis setup.
-    rdb = redis.Redis(connection_pool=redis_pool)
-    pipe = rdb.pipeline(transaction=False)
-
     # Download a random article.
     url = "http://en.wikipedia.org/wiki/Special:Random"
     r = requests.get(url)
@@ -35,18 +30,25 @@ def run_one(*args):
     print(r.url)
 
     # Parse and walk the document.
-    tree = BeautifulSoup(r.text)
+    tree = BeautifulSoup(r.text, "lxml")
+    words = []
     for el in tree.find(id="mw-content-text").find_all("p"):
         for s in tokenizer.tokenize(el.text):
             for t in nltk.pos_tag(nltk.word_tokenize(s)):
                 if t[1][0] == "N" and t[1][-1] != "P":
                     w = re.sub(r"[0-9,.;?\[\]\(\)]", "", t[0].lower())
                     if len(w) > 2:
-                        pipe.zincrby("puppy:words", w, 1)
-    pipe.execute()
+                        words.append(w)
+
+    return r.url, Counter(words)
 
 
 if __name__ == "__main__":
+    fn = "words.txt"
+    counter = Counter()
     pool = Pool()
     for i in range(1024):
-        pool.map(run_one, range(128 * pool._processes))
+        for c in pool.map(run_one, range(128 * pool._processes)):
+            counter.update(c)
+        with open(fn, "w") as f:
+            f.write("\n".join(counter.most_common(2048)))
